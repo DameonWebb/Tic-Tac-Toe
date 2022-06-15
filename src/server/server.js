0a4fadd16b0f4ed98b7db280b108474545e2ef59
DameonWebb <dameonlwebb@gmail.com>
2022-06-15 08:26:02 -0700

    removed files that weren't needed

--- src/server/server.js
+++
@@ -1,8 +0,0 @@
-//create an express server , using assets from the Build folder
-const express = require('express');
-
-const app = express();
-
-app.use(express.static('build'));
-
-module.exports = app;
